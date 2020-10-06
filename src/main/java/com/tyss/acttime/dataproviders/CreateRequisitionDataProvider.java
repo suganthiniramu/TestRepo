4c0fdfd6bdc2f6d94479bbb72c95a825617b677b
ShreyaTestYantra <shreya.u@testyantra.com>
2020-10-06 13:08:27 +0530

    minor changes

--- src/main/java/com/tyss/acttime/dataproviders/CreateRequisitionDataProvider.java
+++ src/main/java/com/tyss/acttime/dataproviders/CreateRequisitionDataProvider.java
@@ -25,25 +25,4 @@ public class CreateRequisitionDataProvider {
 		return workflowdata;
 
 	}
-
-	@DataProvider(name = "LoginDataProvider")
-	public static Object[][] getWorkFlowData() {
-		int noofrows = ExcelUtil.getRowCount(BaseTest.EXCELPATH, "appData");
-		int noofcols = ExcelUtil.getColoumCount(BaseTest.EXCELPATH, "appData");
-		Object[][] workflowdata = new Object[noofrows][noofcols];
-		
-		
-		
-		for (int i = 1; i <= noofrows; i++) {
-			String data[] = ExcelUtil.getRowData(BaseTest.EXCELPATH, "appData", i);
-
-			for (int j = 1; j < data.length-1; j++) {
-
-				workflowdata[i - 1][j] = data[j];
-			}
-
-		}
-		return workflowdata;
-
-	}
 }
